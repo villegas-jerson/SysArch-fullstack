32079e74fd36b0bcb81efa575ce71facf21e34ef
Jerson Villegas <ksbashelor@gmail.com>
2026-03-25 18:08:39 +0800

    database

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$host     = "localhost";
+$host     = "crossover.proxy.rlwy.net:11938";
 $dbname   = "sit-inmonitoring";
 $username = "root";
 $password = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password, $port);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
